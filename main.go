6e104d995cdf35feb349ec24cb5737c2d9459d9d
forchain <tony.zhouhj@gmail.com>
2017-12-27 18:01:48 +0800

    remove redundant argument

--- main.go
+++ main.go
@@ -3,25 +3,16 @@ package main
 import (
 	"github.com/forchain/ethereumbigdata/parsers"
 	"flag"
-	"log"
 )
 
 func main() {
 	out := new(string)
 	rpc := new(string)
 
-	flag.StringVar(rpc, "rpc", "127.0.0.1:8545", "RPC server address")
+	flag.StringVar(rpc, "rpc", "http://127.0.0.1:8545", "RPC server address")
 	flag.StringVar(out, "out", "/tmp/out", "Out path")
 	flag.Parse()
 
-	args := flag.Args()
-	if len(args) == 0 {
-		log.Println("-out OUT_PATH -rpc RPC_SERVER <balance>")
-		return
-	}
-
-	if args[0] == "balance" {
-		p := new(parsers.BalanceParser)
-		p.Parse(*rpc, *out)
-	}
+	p := new(parsers.BalanceParser)
+	p.Parse(*rpc, *out)
 }
